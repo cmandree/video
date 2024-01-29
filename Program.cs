cc568e43baeca0d10c3be6c18abfd92fc8f3fade
cmandree <cmandree@my.okcu.edu>
2024-01-28 18:46:14 -0600

    autoplay

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ class Program
     static void Main(string[] args)
     {
         QRCodeGenerator qrGenerator = new QRCodeGenerator();
-        QRCodeData qrCodeData = qrGenerator.CreateQrCode("https://drive.google.com/file/d/1cvvHeCfOzSuMLXFt9IespWblsJ7uRkgG/view?usp=drive_link", QRCodeGenerator.ECCLevel.Q);
+        QRCodeData qrCodeData = qrGenerator.CreateQrCode("https://cmandree.github.io/video/", QRCodeGenerator.ECCLevel.Q);
         
         //QRCodeData qrCodeData = qrGenerator.CreateQrCode("https://www.youtube.com/watch_popup?v=oKZSXAZWsP0", QRCodeGenerator.ECCLevel.Q);
         //QRCodeData qrCodeData = qrGenerator.CreateQrCode("https://youtu.be/oKZSXAZWsP0?si=0f9rXcFRlVbqfPQK", QRCodeGenerator.ECCLevel.Q);
