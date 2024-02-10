b8c051881add0442efdcb39d89ff491bf00d369c
cmandree <cmandree@my.okcu.edu>
2024-02-10 15:26:37 -0600

    Added css to all medias

--- Program.cs
+++ Program.cs
@@ -11,12 +11,8 @@ class Program
         //QRCodeData qrCodeData = qrGenerator.CreateQrCode("https://www.youtube.com/watch_popup?v=oKZSXAZWsP0", QRCodeGenerator.ECCLevel.Q);
         //QRCodeData qrCodeData = qrGenerator.CreateQrCode("https://youtu.be/oKZSXAZWsP0?si=0f9rXcFRlVbqfPQK", QRCodeGenerator.ECCLevel.Q);
         //QRCodeData qrCodeData = qrGenerator.CreateQrCode("https://youtu.be/oKZSXAZWsP0", QRCodeGenerator.ECCLevel.Q);
-        //QRCodeData qrCodeData = qrGenerator.CreateQrCode("file:///C:/Users/cmand/Documents/Projects/qrcodecmd-main/index.html", QRCodeGenerator.ECCLevel.Q);
         QRCode qrCode = new QRCode(qrCodeData);
         Bitmap qrCodeImage = qrCode.GetGraphic(20);
         qrCodeImage.Save("test.png");
-
-        //testing
-        //<iframe width="560" height="315" src="https://www.youtube.com/embed/oKZSXAZWsP0?si=67ahqT1cxE5hFUTm&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     }
 }
