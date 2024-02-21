2d9d47ddb346c007a7834e03f2eff3cd0c23f756
cmandree <cmandree@my.okcu.edu>
2024-02-20 20:11:03 -0600

    Modified styles

--- Program.cs
+++ Program.cs
@@ -7,12 +7,8 @@ class Program
     {
         QRCodeGenerator qrGenerator = new QRCodeGenerator();
         QRCodeData qrCodeData = qrGenerator.CreateQrCode("https://cmandree.github.io/video/", QRCodeGenerator.ECCLevel.Q);
-        
-        //QRCodeData qrCodeData = qrGenerator.CreateQrCode("https://www.youtube.com/watch_popup?v=oKZSXAZWsP0", QRCodeGenerator.ECCLevel.Q);
-        //QRCodeData qrCodeData = qrGenerator.CreateQrCode("https://youtu.be/oKZSXAZWsP0?si=0f9rXcFRlVbqfPQK", QRCodeGenerator.ECCLevel.Q);
-        //QRCodeData qrCodeData = qrGenerator.CreateQrCode("https://youtu.be/oKZSXAZWsP0", QRCodeGenerator.ECCLevel.Q);
         QRCode qrCode = new QRCode(qrCodeData);
         Bitmap qrCodeImage = qrCode.GetGraphic(20);
-        qrCodeImage.Save("test.png");
+        qrCodeImage.Save("qrCode.png");
     }
 }
